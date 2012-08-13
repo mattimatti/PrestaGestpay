1fee1bab4dd083fca4138a6c47b098b6e48ee33f
andreadepirro <andreadepirro@192.168.1.115>
2012-08-13 18:29:25 +0200

    Rewritten validation.php to be compliant with Prestashop 1.4 (and with analytics modules)

--- validation.php
+++ validation.php
@@ -22,16 +22,58 @@
  * @copyright Andrea De Pirro & Enrico Aillaud
  *
  */
-/* SSL Management */
-$useSSL = false;
 
 include(dirname(__FILE__) . '/../../config/config.inc.php');
-include(dirname(__FILE__) . '/../../header.php');
+include(dirname(__FILE__). '/../../init.php');
+
 include(dirname(__FILE__) . '/gestpay.php');
+global $cookie;
 
 if (!$cookie->isLogged())
   Tools::redirect('authentication.php?back=order.php');
-$gestpay = new GestPay();
-echo $gestpay->validatePayment(trim($_GET['a']), trim($_GET['b']));
 
-include_once(dirname(__FILE__) . '/../../footer.php');
+$gestpay = new GestPay();
+$gestpay_decrypt = $gestpay->deCrypt(trim($_GET['a']), trim($_GET['b']));
+
+//    $shop_login = trim($gestpay_decrypt->GetShopLogin());
+//    $currency = $this->convertToCurrencySymbol($gestpay_decrypt->GetCurrency());
+$amount = floatval($gestpay_decrypt->GetAmount());
+$shop_transaction_id = trim($gestpay_decrypt->GetShopTransactionID());
+//    $buyer_name = preg_replace('#[\W]#', ' ', trim($gestpay_decrypt->GetBuyerName()));
+//    $buyer_email = trim($gestpay_decrypt->GetBuyerEmail());
+$transaction_result = trim($gestpay_decrypt->GetTransactionResult());
+//    $authorization_code = trim($gestpay_decrypt->GetAuthorizationCode());
+$error_code = trim($gestpay_decrypt->GetErrorCode());
+$error_description = trim($gestpay_decrypt->GetErrorDescription());
+//    $error_bank_transaction_id = trim($gestpay_decrypt->GetBankTransactionID());
+//    $alert_code = trim($gestpay_decrypt->GetAlertCode());
+//    $alert_description = trim($gestpay_decrypt->GetAlertDescription());
+//    $custom_info = trim($gestpay_decrypt->GetCustomInfo());
+
+$cart = new Cart((int)$shop_transaction_id);
+$customer = new Customer((int)$cart->id_customer);
+if($transaction_result == 'OK') {
+    $gestpay->validateOrder($shop_transaction_id, Configuration::get('PS_OS_PAYMENT'), $amount, 'GestPay', NULL, NULL, NULL, false, $customer->secure_key);
+}
+else {
+    
+    // @todo use translation function
+    $error_message = "Error " . $error_code . "  " . $error_description;
+    $gestpay->validateOrder($shop_transaction_id, Configuration::get('PS_OS_ERROR'), $amount, 'GestPay', $error_message, NULL, NULL, false, $customer->secure_key);
+    $checkout_type = Configuration::get('PS_ORDER_PROCESS_TYPE') ? 'order-opc' : 'order';
+    $url = _PS_VERSION_ >= '1.5' ? 'index.php?controller=' . $checkout_type . '&' : $checkout_type . '.php?';
+    $url .= 'step=3&cgv=1&message='.$error_message;
+
+    if (!isset($_SERVER['HTTP_REFERER']) || strstr($_SERVER['HTTP_REFERER'], 'order'))
+        Tools::redirect($url);
+    elseif (strstr($_SERVER['HTTP_REFERER'], '?'))
+        Tools::redirect($_SERVER['HTTP_REFERER'].'&gestpayerror=1&message='.$error_message, '');
+    else
+        Tools::redirect($_SERVER['HTTP_REFERER'].'?gestpayerror=1&message='.$error_message, '');
+}
+
+$url = 'index.php?controller=order-confirmation&';
+if (_PS_VERSION_ < '1.5')
+    $url = 'order-confirmation.php?';
+
+Tools::redirect($url . 'id_module=' . (int)$gestpay->id . '&id_cart=' . $shop_transaction_id . '&key=' . $customer->secure_key . '&id_order=' . $gestpay->currentOrder);
